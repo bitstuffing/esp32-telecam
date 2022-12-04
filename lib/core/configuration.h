4348192270ddf592da159a220fefbbb31c77e335
bitstuffing <eduardo.higuero@hotmail.com>
2022-12-04 19:26:24 +0100

    (WIP) migrating to 0.2 and cleaning duplicated parameters from configuration.h

--- lib/core/configuration.h
+++ lib/core/configuration.h
@@ -2,10 +2,5 @@
 
 // you can enter your home chat_id, so the device can send you a reboot message, otherwise it responds to the chat_id talking to telegram
 String chat_id = "999999";
-// your Bot Token (get it from Botfather)
-#define BOTtoken "0000000000:XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX"
-// your network SSID and PASSWORD
-char ssid[] = "SSID";
-char password[] = "WIFI_PASSWD";
 //list of admin users, it must be the Telegram @ID without @
 String authUsers[] = {"username"};
